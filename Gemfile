04ea3502e7a193d19c66b8bd4a01bb933b00edbb
Renato Vieira <guerin16@gmail.com>
2015-11-06 16:59:06 -0500

    Follower and followed

--- Gemfile
+++ Gemfile
@@ -45,8 +45,4 @@ group :development do
   gem 'spring'
 end
 
-gem 'bootstrap-sass', '~> 3.2.0'
-gem 'autoprefixer-rails'
-gem 'bootstrap-generators', '~> 3.3.4'
-
-
+gem 'bootstrap-sass', '~> 3.3.5'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   has_many :collaborates_in
   has_many :playlists, through: :collaborates_in
   
+  has_many :follow, class_name: "Follow", foreign_key: :follower_id
+  has_many :following, through: :follow, source: :following
+  
+  has_many :inverse_follow, class_name: "Follow", foreign_key: :following_id
+  has_many :followers, through: :inverse_follow, source: :follower
+  
   def to_s
     self.username
   end
